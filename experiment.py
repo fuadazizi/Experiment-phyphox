c412a363f15967f6a17aa6039d8188e7f22ec1d9
fuadazizi <60860010+fuadazizi@users.noreply.github.com>
2021-04-11 10:23:24 +0700

    first run
    read and write excel file

--- experiment.py
+++ experiment.py
@@ -1,18 +1,31 @@
 import pandas as pd
 
-# import dan baca file excel
-dfAccel = pd.read_excel('experiment.xlsx', sheetname='Accelerometer')
-dfGyro = pd.read_excel('experiment.xlsx', sheetname='Gyroscope')
+# import and read excel file
+dfAccel = pd.read_excel('experiment.xlsx', sheet_name='Accelerometer')
+dfGyro = pd.read_excel('experiment.xlsx', sheet_name='Gyroscope')
 
-# variabel penampung data accelerometer dan gyroscope
+# variable for accelerometer and gyroscope data
 dataaccel = []
 datagyro = []
 
-
+# save with dictionary
+# Accelerometer part
 for i in dfAccel.index:
 	data = {}
-	data['Time'] = df['Time'][i]
-	data['x'] = df['Acceleration x (m/s^2)'][i]
-	data['y'] = df['Acceleration y (m/s^2)'][i]
-	data['z'] = df['Acceleration z (m/s^2)'][i]
+	data['Time'] = dfAccel['Time (s)'][i]
+	data['x'] = dfAccel['Acceleration x (m/s^2)'][i]
+	data['y'] = dfAccel['Acceleration y (m/s^2)'][i]
+	data['z'] = dfAccel['Acceleration z (m/s^2)'][i]
 	dataaccel.append(data)
+
+# Gyroscope part
+for i in dfGyro.index:
+	data = {}
+	data['Time'] = dfGyro['Time (s)'][i]
+	data['x'] = dfGyro['Gyroscope x (rad/s)'][i]
+	data['y'] = dfGyro['Gyroscope y (rad/s)'][i]
+	data['z'] = dfGyro['Gyroscope z (rad/s)'][i]
+	datagyro.append(data)
+
+print(dataaccel)
+print(datagyro)
