bdecd79b65dce4474d6aac8bb502d76c11a98db2
fuadazizi <60860010+fuadazizi@users.noreply.github.com>
2021-04-11 20:02:14 +0700

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -1,5 +1,7 @@
+# import all the library we need
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 # import and read excel file
 dfAccel = pd.read_excel('experiment.xlsx', sheet_name='Accelerometer')
@@ -20,60 +22,80 @@ for i in dfAccel.index:
 	dataAccel.append(data)
 
 # Gyroscope part
-# I want to use deg/s unit instead rad/s
-# so I convert rad/s unit to deg/s unit
+# I want to use deg/s unit instead rad/s, so I convert rad/s unit to deg/s unit
 for i in dfGyro.index:
 	data = {}
 	data['t'] = dfGyro['Time (s)'][i]
-	data['x'] = dfGyro['Gyroscope x (rad/s)'][i] * 57.2958
-	data['y'] = dfGyro['Gyroscope y (rad/s)'][i] * 57.2958
-	data['z'] = dfGyro['Gyroscope z (rad/s)'][i] * 57.2958
+	data['x'] = math.degrees(dfGyro['Gyroscope x (rad/s)'][i])
+	data['y'] = math.degrees(dfGyro['Gyroscope y (rad/s)'][i])
+	data['z'] = math.degrees(dfGyro['Gyroscope z (rad/s)'][i])
 	dataGyro.append(data)
-
-#print(dataAccel)
-#print(dataGyro)
 
 # plot Accelerometer
 plotX = []
 plotY = []
 plotZ = []
 time = []
+'''
 for data in dataAccel:
 	plotX.append(data['x'])
 	plotY.append(data['y'])
 	plotZ.append(data['z'])
 	time.append(data['t'])
-
-plt.subplot(2,1,1)
-plt.plot(time, plotX, label = "X", linewidth=0.8)
-plt.plot(time, plotY, label = "Y", linewidth=0.8)
-plt.plot(time, plotZ, label = "Z", linewidth=0.8)
-#plt.title('Plot Accelerometer')
+plt.title('Plot Accelerometer')
+#plt.subplot(2,1,1)
+plt.plot(time, plotX, label = "X", linewidth=0.7, color = 'red')
+plt.plot(time, plotY, label = "Y", linewidth=0.7, color = 'green')
+plt.plot(time, plotZ, label = "Z", linewidth=0.7, color = 'blue')
 plt.legend()
 plt.xlabel('Time (s)')
 plt.ylabel('Accelerometer (m/s^2)')
-
+plt.show()
+'''
 # plot Gyroscope
+plotX = []
+plotY = []
+plotZ = []
+time = []
+
 for data in dataGyro:
 	plotX.append(data['x'])
 	plotY.append(data['y'])
 	plotZ.append(data['z'])
 	time.append(data['t'])
 
-plt.subplot(2,1,2)
-plt.plot(time, plotX, label = "X", linewidth=0.8)
-plt.plot(time, plotY, label = "Y", linewidth=0.8)
-plt.plot(time, plotZ, label = "Z", linewidth=0.8)
-#plt.title('Plot Gyroscope')
+plt.title('Plot Gyroscope')
+#plt.subplot(2,1,2)
+#plt.plot(time, plotX, label = "X", linewidth=0.7, color = 'red')
+plt.plot(time, plotY, label = "Y", linewidth=0.7, color = 'green')
+#plt.plot(time, plotZ, label = "Z", linewidth=0.7, color = 'blue')
 plt.legend()
 plt.xlabel('Time (s)')
 plt.ylabel('Gyroscope (deg/s)')
 
 # plot show
 plt.show()
-#keys = dataAccel.keys()
-#values = dataAccel.values()
-#plt.bar(keys,values)
 
-#plt.plot(dataAccel)
-#plt.show
+# Now change accelerometer data to accelerometer angle to know knee flexion
+print("Convert accelerometer to accelerometer angle")
+
+def arctan(x,y,z):
+	return math.degrees(math.atan(abs(z/math.sqrt(y**2 + z**2))))
+
+newAccel = []
+timeAccel = []
+for data in dataAccel:
+	x = data['x']
+	y = data['y']
+	z = data['z']
+	newAccel.append(arctan(x,y,z))
+	timeAccel.append(data['t'])
+
+# plot the new Acceleration data
+plt.title('Plot Accelerometer Angle')
+plt.plot(timeAccel,newAccel,linewidth=0.7)
+plt.xlabel('Time (s)')
+plt.ylabel('Accelerometer angle (degree)')
+plt.show()
+
+#print(newAccel)
