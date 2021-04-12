ffa91b19216a0f8de76c08af14f0a11b83f2e4f4
fuadazizi <60860010+fuadazizi@users.noreply.github.com>
2021-04-12 13:56:43 +0700

    final

--- experiment.py
+++ experiment.py
@@ -11,18 +11,18 @@ dfGyro = pd.read_excel('experiment.xlsx', sheet_name='Gyroscope')
 dataAccel = {}
 dataGyro = {}
 
-# save with dictionary
+# save with dictionary of list
 # Accelerometer part
-time = []
+Ndata = []
 dataX = []
 dataY = []
 dataZ = []
 for i in dfAccel.index:
-	time.append(dfAccel['Time (s)'][i])
+	Ndata.append(i)
 	dataX.append(dfAccel['Acceleration x (m/s^2)'][i])
 	dataY.append(dfAccel['Acceleration y (m/s^2)'][i])
 	dataZ.append(dfAccel['Acceleration z (m/s^2)'][i])
-dataAccel['t'] = time
+dataAccel['n'] = Ndata
 dataAccel['x'] = dataX
 dataAccel['y'] = dataY
 dataAccel['z'] = dataZ
@@ -30,112 +30,96 @@ dataAccel['z'] = dataZ
 # Gyroscope part
 # I want to use deg/s unit instead rad/s, so I convert rad/s unit to deg/s unit
 # with using math.degrees()
-time = []
+Ndata = []
 dataX = []
 dataY = []
 dataZ = []
 for i in dfGyro.index:
-	time.append(dfGyro['Time (s)'][i])
-	dataX.append(dfGyro['Gyroscope x (rad/s)'][i])
-	dataY.append(dfGyro['Gyroscope y (rad/s)'][i])
-	dataZ.append(dfGyro['Gyroscope z (rad/s)'][i])
-dataGyro['t'] = time
+	Ndata.append(i)
+	dataX.append(math.degrees(dfGyro['Gyroscope x (rad/s)'][i]))
+	dataY.append(math.degrees(dfGyro['Gyroscope y (rad/s)'][i]))
+	dataZ.append(math.degrees(dfGyro['Gyroscope z (rad/s)'][i]))
+dataGyro['n'] = Ndata
 dataGyro['x'] = dataX
 dataGyro['y'] = dataY
 dataGyro['z'] = dataZ
 
 # plot Accelerometer
-'''
-plotX = []
-plotY = []
-plotZ = []
-time = []
 
-for data in dataAccel:
-	plotX.append(data['x'])
-	plotY.append(data['y'])
-	plotZ.append(data['z'])
-	time.append(data['t'])
-'''
 plt.title('Plot Accelerometer')
 #plt.subplot(2,1,1)
-plt.plot(dataAccel['t'], dataAccel['x'], label = "X", linewidth=0.7, color = 'red')
-plt.plot(dataAccel['t'], dataAccel['y'], label = "Y", linewidth=0.7, color = 'green')
-plt.plot(dataAccel['t'], dataAccel['z'], label = "Z", linewidth=0.7, color = 'blue')
+plt.plot(dataAccel['n'], dataAccel['x'], label = "X", linewidth=0.7, color = 'red')
+plt.plot(dataAccel['n'], dataAccel['y'], label = "Y", linewidth=0.7, color = 'green')
+plt.plot(dataAccel['n'], dataAccel['z'], label = "Z", linewidth=0.7, color = 'blue')
 plt.legend()
-plt.xlabel('Time (s)')
+plt.xlabel('Number of Data')
 plt.ylabel('Accelerometer (m/s^2)')
 plt.show()
 
 # plot Gyroscope
-'''
-plotX = []
-plotY = []
-plotZ = []
-time = []
 
-for data in dataGyro:
-	plotX.append(data['x'])
-	plotY.append(data['y'])
-	plotZ.append(data['z'])
-	time.append(data['t'])
-'''
 plt.title('Plot Gyroscope')
 #plt.subplot(2,1,2)
-plt.plot(dataGyro['t'], dataGyro['x'], label = "X", linewidth=0.7, color = 'red')
-plt.plot(dataGyro['t'], dataGyro['y'], label = "Y", linewidth=0.7, color = 'green')
-plt.plot(dataGyro['t'], dataGyro['z'], label = "Z", linewidth=0.7, color = 'blue')
+plt.plot(dataGyro['n'], dataGyro['x'], label = "X", linewidth=0.7, color = 'red')
+plt.plot(dataGyro['n'], dataGyro['y'], label = "Y", linewidth=0.7, color = 'green')
+plt.plot(dataGyro['n'], dataGyro['z'], label = "Z", linewidth=0.7, color = 'blue')
 plt.legend()
-plt.xlabel('Time (s)')
+plt.xlabel('Number of Data')
 plt.ylabel('Gyroscope (deg/s)')
-
-# plot show
 plt.show()
 
 # Now change accelerometer data to accelerometer angle to know knee flexion
 print("Convert accelerometer to accelerometer angle")
 
-def arctan(x,y,z):
-	return math.degrees(math.atan(abs(z/math.sqrt(y**2 + z**2))))
-
 newAccel = []
 timeAccel = []
-for i in range(len(dataAccel['t'])):
+for i in range(len(dataAccel['n'])):
 	x = dataAccel['x'][i]
 	y = dataAccel['y'][i]
 	z = dataAccel['z'][i]
-	newAccel.append(arctan(x,y,z))
-	#timeAccel.append(data['t'])
+	newAccel.append(math.degrees(math.atan2(z,y)))
 
 # plot the new Acceleration data
 plt.title('Plot Accelerometer Angle')
-plt.plot(dataAccel['t'],newAccel,linewidth=0.7)
-plt.xlabel('Time (s)')
+plt.plot(dataAccel['n'],newAccel, label = 'Accelerometer angle', linewidth=0.7)
+plt.plot(dataGyro['n'],dataGyro['y'], label = 'Yaw Gyroscope (y-axis)', linewidth=0.7)
+plt.legend()
+plt.xlabel('Number of Data')
 plt.ylabel('Accelerometer angle (degree)')
 plt.show()
 
-#print(newAccel)
+# Now to get angle of gyroscope, more precisely the yaw, we need to integral the gyroscope data of yaw
+print("Complementary Filter Algorithm to know Knee Flexion")
+print("Integral yaw of gyroscope. In my case, the y-axis")
 
-# Now to get angle of gyroscope, we need to integral the gyroscope data
+yawGyro = []
+dt = 0.0147
+for i in range(len(dataGyro['n'])):
+	yawGyro.append(dataGyro['y'][i]*dt)
 
-newGyro = []
-timeGyro = []
-plotX = []
-plotY = []
-plotZ = []
-dt = dataGyro['t'][1] - dataGyro['t'][0]
-for i in range(len(dataGyro['t'])):
-	plotX.append(dataGyro['x'][i]*dt)
-	plotY.append(dataGyro['y'][i]*dt)
-	plotZ.append(dataGyro['z'][i]*dt)
-	#timeGyro.append(data['t'])
+def ComplementaryFilter(yawGyro,angleAccel):
+	cleanAngle = []
+	loop = 0
+	num = []
+	if len(yawGyro) > len(angleAccel):
+		loop = len(angleAccel) 
+	else: 
+		loop = len(yawGyro)
+	for i in range(loop-1):
+		GyroAngle = 0.8 * (yawGyro[i-1] + yawGyro[i])
+		AccPhiAngle = 0.2 * angleAccel[i]
+		cleanAngle.append(GyroAngle + AccPhiAngle)
+		num.append(i)
+	return cleanAngle, num
 
-# plot the new Integral gyroscope data
-plt.title('Plot Gyroscope Angle')
-plt.plot(dataGyro['t'], plotX, label = 'X', linewidth=0.7)
-plt.plot(dataGyro['t'], plotY, label = 'Y',linewidth=0.7)
-plt.plot(dataGyro['t'], plotZ, label = 'Z',linewidth=0.7)
+# plot complementary filter result
+
+gaitAngle,num = ComplementaryFilter(yawGyro,newAccel)
+plt.title('Complementary Filter Result (Knee Flexion)')
+plt.plot(num, gaitAngle, label = 'Gait angle (filtered)', linewidth = 0.7)
+#plt.plot(dataGyro['n'], yawGyro, label = 'Yaw Gyroscope (y-axis)', linewidth = 0.7)
+#plt.plot(dataAccel['n'], newAccel, label = 'Raw Accelerometer', linewidth = 0.7)
 plt.legend()
-plt.xlabel('Time (s)')
-plt.ylabel('Gyroscope angle (degree)')
+plt.xlabel('Number of data')
+plt.ylabel('Angle (degree)')
 plt.show()
